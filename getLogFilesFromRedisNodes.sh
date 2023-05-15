904c15fda590020252e70ce80ed9d492554f44d5
Angel Motta <angel.motta@utec.edu.pe>
2023-05-15 10:21:01 -0500

    refactor: remove old readLog method

--- getLogFilesFromRedisNodes.sh
+++ getLogFilesFromRedisNodes.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-docker cp redis_svr1:/root/mylogs/redislogsvr1.txt logs/sinrabia/t_sample_50_2c/
-docker cp redis_svr2:/root/mylogs/redislogsvr2.txt logs/sinrabia/t_sample_50_2c/
-docker cp redis_svr3:/root/mylogs/redislogsvr3.txt logs/sinrabia/t_sample_50_2c/
+docker cp redis_svr1:/root/mylogs/redissvr1.log logs/sinrabia/t_sample_5000_2/
+docker cp redis_svr2:/root/mylogs/redissvr2.log logs/sinrabia/t_sample_5000_2/
+docker cp redis_svr3:/root/mylogs/redissvr3.log logs/sinrabia/t_sample_5000_2/
 
 # Monitor
 # On svr1
-# redis-cli -p 6380 monitor | tee redislogsvr1.txt
+# redis-cli -p 6380 monitor | tee redissvr1.log
 
 # On svr2
-# redis-cli -p 6381 monitor | tee redislogsvr2.txt
+# redis-cli -p 6381 monitor | tee redissvr2.log
 
 # On svr3
-# redis-cli -p 6382 monitor | tee redislogsvr3.txt
+# redis-cli -p 6382 monitor | tee redissvr3.log
--- myRabiaLogAnalyzer.py
+++ myRabiaLogAnalyzer.py
@@ -11,73 +11,8 @@ REDIS_LOGFILE = "redislog"
 RABIA_LOGFILE = "rabialog"
 LOG_EXTENSION = ".log"
 
-def oldreadLogFiles(logfile1, logfile2, logfile3):
-    f1 = open(logfile1)
-    f2 = open(logfile2)
-    f3 = open(logfile3)
-
-    csv_f1 = csv.reader(f1, delimiter=" ")
-    csv_f2 = csv.reader(f2, delimiter=" ")
-    csv_f3 = csv.reader(f3, delimiter=" ")
-
-    countDiff = 0
-    numLine = 1
-    setCmds = 0
-    getCmds = 0
-    for rowf1, rowf2, rowf3 in zip_longest(csv_f1, csv_f2, csv_f3):
-        numLine += 1
-        if len(rowf1) <= 1: continue    # Blank line [] or ['OK']
-        if rowf1[3] == "hello" or rowf1[3] == "ping": continue
-
-        # idx '3' is type operation: set | get
-        opR1, opR2, opR3 = rowf1[3], rowf2[3], rowf3[3]
-        if opR1 != opR2 or opR1 != opR3 or opR2 != opR3:
-            print("Tipo Operacion diferente: Op# " + str(numLine))
-            countDiff += 1
-            continue
-
-        # At this point all replicas have the same kind of operation
-        # If operation is Get -> inspect consistency
-        if len(rowf1) == 5:
-            # Get Command
-            getCmds += 1
-            # Check key of get operation
-            keyR1, KeyR2, KeyR3 = rowf1[4], rowf2[4], rowf3[4]
-            if keyR1 != KeyR2 or keyR1 != KeyR3 or KeyR2 != KeyR3:
-                print("Operacion GET diferente: Op# " + str(numLine))
-                countDiff += 1
-                continue
-        # If operations is Set -> inspect consistency
-        elif len(rowf1) == 6:
-            # Set Command
-            setCmds += 1
-            # Check key of set operation
-            keyR1, KeyR2, KeyR3 = rowf1[4], rowf2[4], rowf3[4]
-            if keyR1 != KeyR2 or keyR1 != KeyR3 or KeyR2 != KeyR3:
-                print("Operacion SET diferente key: Op# " + str(numLine))
-                countDiff += 1
-                continue
-            # Check value of set operation
-            valR1, valR2, valR3 = rowf1[5], rowf2[5], rowf3[5]
-            if valR1 != valR2 or valR1 != valR3 or valR2 != valR3:
-                print("Operacion SET diferente value: Op# " + str(numLine))
-                countDiff += 1
-                continue
-        print(rowf1)
-        numLine += 1
-
-    print("Total Sets Commands:" + str(setCmds))
-    print("Total Gets Commands:" + str(getCmds))
-    print("Total Commands: " + str(getCmds + setCmds))
-    print("Total Inconsistencias: " + str(countDiff))
-
-    f1.close()
-    f2.close()
-    f3.close()
-    return
-
 '''
-newreadLogFiles: read log files and generate a list of operations executed by each replica
+readLogFiles: read log files and generate a list of operations executed by each replica
 input: list of log files
 output: decisions of each replicas. Eg [decisionsR1, decisionsR2, decisionsR3]
 '''
